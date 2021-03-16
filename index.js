183ddc4ea72a71b2f8b96fa9f63214242fd09c2f
Mollie Dickenson <molliedickenson@gmail.com>
2021-03-16 10:50:34 -0400

    commit tree 3d556ee9ff6ff0ec95a5befc850b820aee7950d5

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+random;
+
+let num3 = 18;
+let num4 = 7;
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(5, 10, 15, 20);
+max;
+
